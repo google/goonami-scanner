f70bba8b2aa15339a16433e02f525b619181d0cc
tooryx <pprc@google.com>
2026-01-16 14:52:07 +0000

    stage the core engine configuration handler

---
+++ core/config/config.go
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package config provides utilities to load and parse the Goonami config.
+package config
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"github.com/google/goonami-scanner/core/log"
+	"google.golang.org/protobuf/encoding/prototext"
+
+	cpb "github.com/google/goonami-scanner/core/config/config_go_proto"
+)
+
+// ErrUninitialized is returned when a method is called before the configuration is initialized.
+// This happens if `CreateDirectories` was never called.
+var ErrUninitialized = errors.New("configuration was never initialized with CreateDirectories()")
+
+// Config is the configuration used by Goonami. It holds configuration for clients, plugins and
+// options that are global to the scanner. It also keeps track of a set of directories used by the
+// scanner to write files to.
+//
+// The recommend way to use config is:
+//
+// cfg, err := config.FromFile(path)
+// if err != nil { handle errors }
+//
+// cfg.CreateDirectories(workdir)
+// if err != nil { handle errors }
+//
+// defer cfg.Close()
+//
+// Then you can use the configuration
+type Config struct {
+	proto        *cpb.Config
+	workDir      string
+	tempDir      string
+	artifactsDir string
+}
+
+// FromFile creates a new Config from a textproto file. Caller is still responsible for explicitly
+// calling `CreateDirectories` before using the configuration.
+func FromFile(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	cfgpb := &cpb.Config{}
+	if err := prototext.Unmarshal(data, cfgpb); err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		proto: cfgpb,
+	}, nil
+}
+
+// FromProto creates a new Config from a Config proto. Caller is still responsible for explicitly
+// calling `CreateDirectories` before using the configuration.
+func FromProto(cfgpb *cpb.Config) *Config {
+	return &Config{
+		proto: cfgpb,
+	}
+}
+
+// CreateDirectories that are used by the scanner throughout its execution. This method MUST be
+// called before using the configuration as it makes a set of directories available to the scanner
+// and its plugins.
+func (c *Config) CreateDirectories(workdir string) error {
+	if workdir == "" {
+		return fmt.Errorf("working directory is empty")
+	}
+
+	if _, err := os.Stat(workdir); err != nil {
+		return fmt.Errorf("error while accessing working directory %q: %w", workdir, err)
+	}
+
+	c.workDir = workdir
+	c.tempDir = path.Join(c.workDir, "tempfiles")
+	if err := os.MkdirAll(c.tempDir, 0700); err != nil {
+		return err
+	}
+
+	c.artifactsDir = path.Join(c.workDir, "artifacts")
+	if err := os.MkdirAll(c.artifactsDir, 0700); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Close ensures proper clean-up is performed.
+func (c *Config) Close() error {
+	if c.tempDir == "" {
+		log.Warn("Clean-up could not be performed. Configuration was never Initialized.")
+		return ErrUninitialized
+	}
+
+	return os.RemoveAll(c.tempDir)
+}
+
+// GlobalConfig returns configuration options that are global.
+func (c *Config) GlobalConfig() *cpb.GlobalConfig {
+	return c.proto.GetGlobalcfg()
+}
+
+// ClientsConfig returns configuration options that are specific to clients.
+func (c *Config) ClientsConfig() *cpb.ClientsConfig {
+	return c.proto.GetClients()
+}
+
+// PluginsConfig returns the configuration options that are specific to plugins.
+func (c *Config) PluginsConfig() *cpb.PluginsConfig {
+	return c.proto.GetPlugins()
+}
+
+// WorkingDirectory that the scanner is using to write files to.
+func (c *Config) WorkingDirectory() string {
+	return c.workDir
+}
+
+// TempDirectory that the scanner is using to write temporary files to.
+func (c *Config) TempDirectory() string {
+	return c.tempDir
+}
+
+// ArtifactsDirectory that the scanner is using to write files that will be available after the
+// scan (artifacts).
+func (c *Config) ArtifactsDirectory() string {
+	return c.artifactsDir
+}
+
+// TimeoutPerRequest is a helper that returns the timeout set in the global configuration.
+func (c *Config) TimeoutPerRequest() time.Duration {
+	return time.Duration(c.GlobalConfig().GetPerformance().GetTimeoutPerRequestSeconds()) * time.Second
+}
---
+++ core/config/config_test.go
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import (
+	"errors"
+	"os"
+	"path"
+	"testing"
+	"time"
+)
+
+const (
+	validConfig   = "testdata/valid.textproto"
+	invalidConfig = "testdata/invalid.textproto"
+)
+
+func TestFromFile(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		wantErr bool
+	}{
+		{
+			name:    "valid_config",
+			path:    validConfig,
+			wantErr: false,
+		},
+		{
+			name:    "invalid_config",
+			path:    invalidConfig,
+			wantErr: true,
+		},
+		{
+			name:    "file_not_found",
+			path:    "not/existing/path",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := FromFile(tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCreateDirectories(t *testing.T) {
+	tests := []struct {
+		name string
+		// setup is a function run before CreateDirectories() is called. It returns the working
+		// directory that should be passed to CreateDirectories().
+		setup   func(t *testing.T) string
+		wantErr bool
+	}{
+		{
+			name: "valid_workdir_creates_all_directories",
+			setup: func(t *testing.T) string {
+				t.Helper()
+				return t.TempDir()
+			},
+			wantErr: false,
+		},
+		{
+			name: "empty_workdir_returns_error",
+			setup: func(t *testing.T) string {
+				t.Helper()
+				return ""
+			},
+			wantErr: true,
+		},
+		{
+			name: "tempdir_creation_fails_because_file_exists",
+			setup: func(t *testing.T) string {
+				t.Helper()
+				dir := t.TempDir()
+				if err := os.WriteFile(path.Join(dir, "tempfiles"), []byte(""), 0644); err != nil {
+					t.Fatalf("cannot create tempfiles file: %v", err)
+				}
+				return dir
+			},
+			wantErr: true,
+		},
+		{
+			name: "workdir_does_not_exist_returns_error",
+			setup: func(t *testing.T) string {
+				t.Helper()
+				return path.Join("path", "to", "a", "non-existing", "directory")
+			},
+			wantErr: true,
+		},
+		{
+			name: "artifactsdir_creation_fails_because_file_exists",
+			setup: func(t *testing.T) string {
+				t.Helper()
+				dir := t.TempDir()
+				if err := os.WriteFile(path.Join(dir, "artifacts"), []byte(""), 0644); err != nil {
+					t.Fatalf("cannot create artifacts file: %v", err)
+				}
+				return dir
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg, err := FromFile(validConfig)
+			if err != nil {
+				t.Fatalf("Failed to load config for test: %v", err)
+			}
+
+			workDir := tt.setup(t)
+
+			if err = cfg.CreateDirectories(workDir); (err != nil) != tt.wantErr {
+				t.Errorf("CreateDirectories() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			if _, err := os.Stat(cfg.WorkingDirectory()); err != nil {
+				t.Errorf("Working directory %q not created: %v", cfg.WorkingDirectory(), err)
+			}
+			if _, err := os.Stat(cfg.TempDirectory()); err != nil {
+				t.Errorf("Temp directory %q not created: %v", cfg.TempDirectory(), err)
+			}
+			if _, err := os.Stat(cfg.ArtifactsDirectory()); err != nil {
+				t.Errorf("Artifacts directory %q not created: %v", cfg.ArtifactsDirectory(), err)
+			}
+		})
+	}
+}
+
+func TestClose(t *testing.T) {
+	tests := []struct {
+		name       string
+		initialize bool
+		wantErr    error
+	}{
+		{
+			name:       "initialized_config_removes_tempdir",
+			initialize: true,
+			wantErr:    nil,
+		},
+		{
+			name:       "uninitialized_config_returns_error",
+			initialize: false,
+			wantErr:    ErrUninitialized,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg, err := FromFile(validConfig)
+			if err != nil {
+				t.Fatalf("Failed to load config for test: %v", err)
+			}
+
+			if tt.initialize {
+				if err := cfg.CreateDirectories(t.TempDir()); err != nil {
+					t.Fatalf("Failed to create directories: %v", err)
+				}
+			}
+
+			tempDir := cfg.TempDirectory()
+			if err = cfg.Close(); !errors.Is(err, tt.wantErr) {
+				t.Fatalf("Close() error = %v, wantErr = %v", err, tt.wantErr)
+			}
+
+			if tt.initialize && tt.wantErr == nil {
+				if _, err := os.Stat(tempDir); err == nil || !errors.Is(err, os.ErrNotExist) {
+					t.Errorf("Temp directory %s not removed after Close()", tempDir)
+				}
+			}
+		})
+	}
+}
+
+func TestWorkingDirectoryIsCorrect(t *testing.T) {
+	workDir := t.TempDir()
+	cfg, err := FromFile(validConfig)
+	if err != nil {
+		t.Fatalf("Failed to load config: %v", err)
+	}
+	if err := cfg.CreateDirectories(workDir); err != nil {
+		t.Fatalf("Failed to create directories: %v", err)
+	}
+	defer cfg.Close()
+
+	if got := cfg.WorkingDirectory(); got != workDir {
+		t.Errorf("WorkingDirectory() = %v, want %v", got, workDir)
+	}
+}
+
+func TestTempDirectoryIsCorrect(t *testing.T) {
+	workDir := t.TempDir()
+	cfg, err := FromFile(validConfig)
+	if err != nil {
+		t.Fatalf("Failed to load config: %v", err)
+	}
+	if err := cfg.CreateDirectories(workDir); err != nil {
+		t.Fatalf("Failed to create directories: %v", err)
+	}
+	defer cfg.Close()
+
+	want := path.Join(workDir, "tempfiles")
+	if got := cfg.TempDirectory(); got != want {
+		t.Errorf("TempDirectory() = %v, want %v", got, want)
+	}
+}
+
+func TestArtifactsDirectoryIsCorrect(t *testing.T) {
+	workDir := t.TempDir()
+	cfg, err := FromFile(validConfig)
+	if err != nil {
+		t.Fatalf("Failed to load config: %v", err)
+	}
+	if err := cfg.CreateDirectories(workDir); err != nil {
+		t.Fatalf("Failed to create directories: %v", err)
+	}
+	defer cfg.Close()
+
+	want := path.Join(workDir, "artifacts")
+	if got := cfg.ArtifactsDirectory(); got != want {
+		t.Errorf("ArtifactsDirectory() = %v, want %v", got, want)
+	}
+}
+
+func TestTimeoutPerRequest(t *testing.T) {
+	cfg, err := FromFile(validConfig)
+	if err != nil {
+		t.Fatalf("Failed to load config: %v", err)
+	}
+	if err := cfg.CreateDirectories(t.TempDir()); err != nil {
+		t.Fatalf("Failed to create directories: %v", err)
+	}
+	defer cfg.Close()
+
+	if got, want := cfg.TimeoutPerRequest(), 30*time.Second; got != want {
+		t.Errorf("TimeoutPerRequest() = %v, want %v", got, want)
+	}
+}
