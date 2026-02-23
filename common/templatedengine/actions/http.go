8a231a1d4636c6dce4da39282821436cd2d47069
Pierre Precourt <pprecourt@google.com>
2026-02-23 05:56:47 -0800

    Add the templated plugin action handler for HTTP requests.
    PiperOrigin-RevId: 874029191

---
+++ common/templatedengine/actions/http.go
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package actions
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/google/goonami-scanner/common/templatedengine/environment"
+	"github.com/google/goonami-scanner/core/log"
+	goohttp "github.com/google/goonami-scanner/core/net/http"
+	"github.com/google/goonami-scanner/core/net/netservice"
+
+	tpb "github.com/google/tsunami-security-scanner-plugins/templated/templateddetector/proto/templated_plugin_go_proto"
+	nspb "github.com/google/tsunami-security-scanner/proto/go/network_service_go_proto"
+)
+
+// HTTPActionRunner runs HTTP actions.
+type HTTPActionRunner struct {
+	client goohttp.Client
+}
+
+// NewHTTPActionRunner creates a new HTTPActionRunner.
+func NewHTTPActionRunner(client goohttp.Client) *HTTPActionRunner {
+	return &HTTPActionRunner{client: client}
+}
+
+// Run executes the HTTP action and return whether it was successful.
+func (r *HTTPActionRunner) Run(ctx context.Context, service *nspb.NetworkService, action *tpb.PluginAction, env *environment.Environment) bool {
+	httpAction := action.GetHttpRequest()
+	if httpAction == nil {
+		log.ErrorContextf(ctx, "action '%s' is not an HTTP action", action.GetName())
+		return false
+	}
+
+	for _, uri := range httpAction.GetUri() {
+		if r.runWithURI(ctx, service, action, env, uri) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (r *HTTPActionRunner) runWithURI(ctx context.Context, service *nspb.NetworkService, action *tpb.PluginAction, env *environment.Environment, uri string) bool {
+	uri = env.Substitute(ctx, uri)
+	uri = strings.TrimPrefix(uri, "/")
+	webRoot, err := netservice.BuildWebRoot(service)
+	if err != nil {
+		log.ErrorContextf(ctx, "failed to build web root: %v", err)
+		return false
+	}
+
+	targetURL := webRoot + "/" + uri
+	httpAction := action.GetHttpRequest()
+
+	method := httpAction.GetMethod().String()
+	if httpAction.GetMethod() == tpb.HttpAction_METHOD_UNSPECIFIED {
+		log.ErrorContextf(ctx, "action '%s' has unspecified HTTP method", action.GetName())
+		return false
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, targetURL, nil)
+	if err != nil {
+		log.ErrorContextf(ctx, "failed to create request: %v", err)
+		return false
+	}
+
+	for _, header := range httpAction.GetHeaders() {
+		req.Header.Add(header.GetName(), env.Substitute(ctx, header.GetValue()))
+	}
+
+	if data := httpAction.GetData(); data != "" {
+		substitutedData := env.Substitute(ctx, data)
+		req.Body = io.NopCloser(strings.NewReader(substitutedData))
+		req.ContentLength = int64(len(substitutedData))
+	}
+
+	resp, err := goohttp.DefaultClient().Do(req)
+	if err != nil {
+		if !httpAction.GetClientOptions().GetIgnoreHttpClientErrors() {
+			log.ErrorContextf(ctx, "HTTP request failed for action '%s': %v", action.GetName(), err)
+		}
+		return false
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.ErrorContextf(ctx, "failed to read response body: %v", err)
+		return false
+	}
+	bodyString := string(bodyBytes)
+
+	if expectedStatus := httpAction.GetResponse().GetHttpStatus(); expectedStatus != 0 {
+		if int64(resp.StatusCode) != expectedStatus {
+			log.DebugContextf(ctx, log.DebugLevelRequest, "workflow failed: expected status code %d, got %d", expectedStatus, resp.StatusCode)
+			return false
+		}
+	}
+
+	if !r.checkExpectations(ctx, resp, bodyString, httpAction, env) {
+		return false
+	}
+
+	if !r.performExtractions(ctx, resp, bodyString, httpAction, env) {
+		return false
+	}
+
+	return true
+}
+
+func (r *HTTPActionRunner) checkExpectations(ctx context.Context, resp *http.Response, body string, httpAction *tpb.HttpAction, env *environment.Environment) bool {
+	response := httpAction.GetResponse()
+	if expectAll := response.GetExpectAll(); expectAll != nil {
+		for _, cond := range expectAll.GetConditions() {
+			if !r.checkExpectation(ctx, resp, body, cond, env) {
+				log.DebugContextf(ctx, log.DebugLevelRequest, "expectation failed: %v", cond)
+				return false
+			}
+		}
+		return true
+	}
+
+	if expectAny := response.GetExpectAny(); expectAny != nil {
+		for _, cond := range expectAny.GetConditions() {
+			if r.checkExpectation(ctx, resp, body, cond, env) {
+				return true
+			}
+		}
+		log.DebugContextf(ctx, log.DebugLevelRequest, "all expectations failed")
+		return false
+	}
+	return true
+}
+
+func (r *HTTPActionRunner) checkExpectation(ctx context.Context, resp *http.Response, body string, cond *tpb.HttpAction_HttpResponse_Expectation, env *environment.Environment) bool {
+	contains := env.Substitute(ctx, cond.GetContains())
+	if cond.GetBody() != nil {
+		return strings.Contains(body, contains)
+	}
+	if header := cond.GetHeader(); header != nil {
+		headerName := env.Substitute(ctx, header.GetName())
+		return strings.Contains(resp.Header.Get(headerName), contains)
+	}
+	return false
+}
+
+func (r *HTTPActionRunner) performExtractions(ctx context.Context, resp *http.Response, body string, httpAction *tpb.HttpAction, env *environment.Environment) bool {
+	response := httpAction.GetResponse()
+	if extractAll := response.GetExtractAll(); extractAll != nil {
+		for _, ext := range extractAll.GetPatterns() {
+			if !r.performExtraction(ctx, resp, body, ext, env) {
+				log.DebugContextf(ctx, log.DebugLevelRequest, "extraction failed: %v", ext)
+				return false
+			}
+		}
+		return true
+	}
+	if extractAny := response.GetExtractAny(); extractAny != nil {
+		for _, ext := range extractAny.GetPatterns() {
+			if r.performExtraction(ctx, resp, body, ext, env) {
+				return true
+			}
+		}
+		log.DebugContextf(ctx, log.DebugLevelRequest, "all extractions failed")
+		return false
+	}
+	return true
+}
+
+func (r *HTTPActionRunner) performExtraction(ctx context.Context, resp *http.Response, body string, ext *tpb.HttpAction_HttpResponse_Extract, env *environment.Environment) bool {
+	varName := ext.GetVariableName()
+	pattern := env.Substitute(ctx, ext.GetRegexp())
+
+	if ext.GetFromBody() != nil {
+		return env.Extract(ctx, body, varName, pattern)
+	}
+	if fromHeader := ext.GetFromHeader(); fromHeader != nil {
+		headerName := env.Substitute(ctx, fromHeader.GetName())
+		headerValue := resp.Header.Get(headerName)
+		return env.Extract(ctx, headerValue, varName, pattern)
+	}
+	return false
+}
---
+++ common/templatedengine/actions/http_test.go
@@ -0,0 +1,278 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package actions
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/google/goonami-scanner/common/templatedengine/environment"
+	"github.com/google/goonami-scanner/core/config"
+	cpb "github.com/google/goonami-scanner/core/config/config_go_proto"
+	goohttp "github.com/google/goonami-scanner/core/net/http"
+
+	tpb "github.com/google/tsunami-security-scanner-plugins/templated/templateddetector/proto/templated_plugin_go_proto"
+	npb "github.com/google/tsunami-security-scanner/proto/go/network_go_proto"
+	nspb "github.com/google/tsunami-security-scanner/proto/go/network_service_go_proto"
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+func TestHTTPActionRunner_Run(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/OK" {
+			w.Header().Set("X-Test", "Value")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("body content"))
+		} else if r.URL.Path == "/POST" {
+			if r.Method != "POST" {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+				return
+			}
+			body, _ := io.ReadAll(r.Body)
+			if string(body) == "expected body" && r.Header.Get("X-Custom") == "custom value" {
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprint(w, "post success")
+			} else {
+				w.WriteHeader(http.StatusBadRequest)
+			}
+		} else if r.URL.Path == "/MULTI_KO" {
+			w.WriteHeader(http.StatusNotFound)
+		} else if r.URL.Path == "/MULTI_OK" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, "multi success")
+		} else if r.URL.Path == "/EXPECT_ANY" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, "found second")
+		} else if r.URL.Path == "/EXTRACT_ANY" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, "key=value")
+		} else if r.URL.Path == "/HEADER_EXPECT" {
+			w.Header().Set("X-Status", "Ready")
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
+	defer ts.Close()
+
+	u, _ := url.Parse(ts.URL)
+	port, _ := strconv.Atoi(u.Port())
+
+	cfg := config.FromProto(&cpb.Config{})
+	goohttp.InitializeDefaults(cfg)
+
+	service := nspb.NetworkService_builder{
+		NetworkEndpoint: npb.NetworkEndpoint_builder{
+			Hostname: npb.Hostname_builder{Name: u.Hostname()}.Build(),
+			Port:     npb.Port_builder{PortNumber: uint32(port)}.Build(),
+		}.Build(),
+		SupportedHttpMethods: []string{"GET", "POST"},
+	}.Build()
+
+	tests := []struct {
+		name       string
+		actionFile string
+		env        func() *environment.Environment
+		wantOk     bool
+		wantVars   map[string]string
+	}{
+		{
+			name:       "when_request_succeeds_returns_true",
+			actionFile: "testdata/request_succeeds.textproto",
+			wantOk:     true,
+		},
+		{
+			name:       "when_status_code_mismatch_returns_false",
+			actionFile: "testdata/status_code_mismatch.textproto",
+			wantOk:     false,
+		},
+		{
+			name:       "when_expectation_all_fails_returns_false",
+			actionFile: "testdata/expectation_all_fails.textproto",
+			wantOk:     false,
+		},
+		{
+			name:       "when_expectation_missing_header_or_body_fails_returns_false",
+			actionFile: "testdata/expect_missing_body_header.textproto",
+			wantOk:     false,
+		},
+		{
+			name:       "when_expectation_any_fails_returns_false",
+			actionFile: "testdata/expectation_any_fails.textproto",
+			wantOk:     false,
+		},
+		{
+			name:       "when_extraction_succeeds_stores_variable",
+			actionFile: "testdata/extract_all_succeeds.textproto",
+			wantOk:     true,
+			wantVars: map[string]string{
+				"extracted": "content",
+			},
+		},
+		{
+			name:       "when_extraction_all_fails_returns_false",
+			actionFile: "testdata/extract_all_fails.textproto",
+			wantOk:     false,
+		},
+		{
+			name:       "when_extraction_missing_header_or_body_fails_returns_false",
+			actionFile: "testdata/extract_missing_header_body.textproto",
+			wantOk:     false,
+		},
+		{
+			name:       "when_header_extraction_succeeds_stores_variable",
+			actionFile: "testdata/header_extraction_succeeds.textproto",
+			wantOk:     true,
+			wantVars: map[string]string{
+				"header_var": "alue",
+			},
+		},
+		{
+			name:       "when_method_is_unspecified_returns_false",
+			actionFile: "testdata/method_unspecified.textproto",
+			wantOk:     false,
+		},
+		{
+			name:       "when_post_with_substitution_works",
+			actionFile: "testdata/post_with_substitution.textproto",
+			env: func() *environment.Environment {
+				e := environment.New()
+				e.Set("path", "POST")
+				e.Set("header_val", "custom value")
+				e.Set("target", "body")
+				return e
+			},
+			wantOk: true,
+		},
+		{
+			name:       "when_multiple_uris_first_fails_second_succeeds_returns_true",
+			actionFile: "testdata/multiple_uris.textproto",
+			wantOk:     true,
+		},
+		{
+			name:       "when_expect_any_works",
+			actionFile: "testdata/expect_any_works.textproto",
+			wantOk:     true,
+		},
+		{
+			name:       "when_expect_all_works",
+			actionFile: "testdata/expect_all_works.textproto",
+			wantOk:     true,
+		},
+		{
+			name:       "when_extract_any_works",
+			actionFile: "testdata/extract_any_works.textproto",
+			wantOk:     true,
+			wantVars:   map[string]string{"v2": "value"},
+		},
+		{
+			name:       "when_header_expectation_works",
+			actionFile: "testdata/header_expectation_works.textproto",
+			wantOk:     true,
+		},
+		{
+			name:       "when_extraction_does_not_compile_returns_false",
+			actionFile: "testdata/extraction_does_not_compile.textproto",
+			wantOk:     false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			action := loadAction(t, tc.actionFile)
+			env := environment.New()
+			if tc.env != nil {
+				env = tc.env()
+			}
+
+			runner := NewHTTPActionRunner(goohttp.DefaultClient())
+			gotOk := runner.Run(context.Background(), service, action, env)
+			if gotOk != tc.wantOk {
+				t.Errorf("Run() = %v, want %v", gotOk, tc.wantOk)
+			}
+
+			for k, v := range tc.wantVars {
+				val, ok := env.Get(k)
+				if !ok || val != v {
+					t.Errorf("env.Get(%q) = %q, %v, want %q, true", k, val, ok, v)
+				}
+			}
+		})
+	}
+}
+
+func loadAction(t *testing.T, filename string) *tpb.PluginAction {
+	t.Helper()
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		t.Fatalf("Failed to read action file %s: %v", filename, err)
+	}
+	action := &tpb.PluginAction{}
+	if err := prototext.Unmarshal(content, action); err != nil {
+		t.Fatalf("Failed to unmarshal action from %s: %v", filename, err)
+	}
+	return action
+}
+
+func TestHTTPActionRunner_Run_Errors(t *testing.T) {
+	service := nspb.NetworkService_builder{
+		NetworkEndpoint: npb.NetworkEndpoint_builder{
+			Hostname: npb.Hostname_builder{Name: "localhost"}.Build(),
+			Port:     npb.Port_builder{PortNumber: 80}.Build(),
+		}.Build(),
+		SupportedHttpMethods: []string{"GET"},
+	}.Build()
+
+	tests := []struct {
+		name       string
+		actionFile string
+	}{
+		{
+			name:       "when_client_do_fails_returns_false",
+			actionFile: "testdata/simple_get.textproto",
+		},
+		{
+			name:       "when_client_do_fails_but_ignored_returns_true",
+			actionFile: "testdata/get_with_ignore_errors.textproto",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			runner := NewHTTPActionRunner(&errorClient{})
+			action := loadAction(t, tc.actionFile)
+
+			if runner.Run(context.Background(), service, action, environment.New()) {
+				t.Error("Run() = true, want false on client error")
+			}
+		})
+	}
+}
+
+type errorClient struct{}
+
+func (c *errorClient) Do(req *http.Request) (*http.Response, error) {
+	return nil, fmt.Errorf("simulated error")
+}
+func (c *errorClient) Initialize(cfg *config.Config) error { return nil }
